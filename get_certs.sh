d2ab5de14ce36b7b6a394307fb2322d5c7e95283
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-07-20 14:37:19 -0700

    revert back to 3.2.1

--- get_certs.sh
+++ get_certs.sh
@@ -20,9 +20,9 @@ generate_cert() {
   echo "I will try to get the ${DB_HOST}-1 cert"
   echo "Connecting to ${DB_HOST}:${DB_PORT}"
 
-  openssl s_client -connect "${DB_HOST}:${DB_PORT}" -showcerts </dev/null | openssl x509 -outform pem >"$cert_folder/${DB_HOST}.pem"
-  openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/${DB_HOST}.der"
-  keytool -import -alias "${DB_HOST}" -keystore $cert_folder/$cert_file -file "$cert_folder/${DB_HOST}.der" -storepass "${certSecret}" -noprompt
+  openssl s_client -connect "${DB_HOST}:${DB_PORT}" -showcerts </dev/null | openssl x509 -outform pem >"$cert_folder/${DB_HOST}.pem" || exit 1
+  openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/${DB_HOST}.der" || exit 1
+  keytool -import -alias "${DB_HOST}" -keystore $cert_folder/$cert_file -file "$cert_folder/${DB_HOST}.der" -storepass "${certSecret}" -noprompt || exit 1
 
   echo "Generated $cert_file and copied it to $cert_folder."
 }
